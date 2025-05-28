2cbcafc58944c6a3e6ddd599048ccfeaafeeb54a
Lucas Paulo <104645155+LPDP22@users.noreply.github.com>
2025-05-28 16:59:53 -0300

    Delete ActualFlask directory

--- ActualFlask/app/config.py
+++
@@ -1,7 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///project.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-class TestingConfig(Config):
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
